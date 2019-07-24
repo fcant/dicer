f473562adf93cefab9c828459d97d806ee53453d
fcant <fabio.canterino@mail.de>
2019-07-24 16:35:06 +0200

    v0.5

--- python/tkinter_GUI.py
+++ python/tkinter_GUI.py
@@ -9,6 +9,8 @@ import time
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
+starting = 0
+
 root = Tk()
 root.title('Dicer')
 
@@ -47,12 +49,12 @@ steptime = 0.0003
 #    stand = stand + 1
 #    labelZahl.config(text=str(stand))
     
-    
+taking_image = 0
 stepper_running = 0
 imgshow_running = 0
-ready_for_img = 0
 
 class stepperThread (threading.Thread):
+
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
@@ -60,27 +62,32 @@ class stepperThread (threading.Thread):
         self.counter = counter
     def run(self):
         global stepper_running
-        global ready_for_img
-        ready_for_img = 0
-        stepper_running = 1
+        global taking_image
+        #print ("============Starting " + self.name)
         stepper()
-        ready_for_img = 1
-        print ("============Exiting " + self.name)
+
+
+        taking_image = 1
+        stepper_running = 0
       
-class showInputThread (threading.Thread):
+        #print ("============Exiting " + self.name)
+
+class liveView (threading.Thread):
+    global imgshow_running
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.counter = counter
     def run(self):
-        global imgshow_running
         imgshow_running = 1
+        #print ("============Starting " + self.name)
         grey = get_image()
-        show_input(grey)
+        show_raw()
         keypoint_img = img_processing(grey)
         number = counting(keypoint_img)
-        show_output(keypoint_img)
+        show_output()
+        #print ("============Exiting " + self.name)
         imgshow_running = 0
       
 
@@ -126,7 +133,7 @@ def step_minus():
 
 def get_image():
     
-    for i in range(2):
+    for i in range(5):
         ret, frame = cap.read()
    
     y=200
@@ -137,12 +144,16 @@ def get_image():
 
     frame1 = frame[y:y + h, x:x + w]  
     grey = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    
+
+    cv2.imwrite('last_raw.png', grey)    
+    
     return grey
 
 
-def img_processing(grey):
+def img_processing(imageinput):
     
-    ret, binary_image = cv2.threshold(grey, 230, 255, cv2.THRESH_BINARY)
+    ret, binary_image = cv2.threshold(imageinput, 230, 255, cv2.THRESH_BINARY)
 
     kernel = np.ones((6, 6), np.uint8)
 
@@ -160,6 +171,18 @@ def img_processing(grey):
     mask = np.zeros((h + 2, w + 2), np.uint8)
     cv2.floodFill(closing, mask, (0, 0), 255);
     cv2.floodFill(closing, mask, (0, 200), 255);
+    
+    cv2.imwrite('last_output.png', closing)
+    
+    
+ 
+    
+    #cv2image2 = cv2.cvtColor(closing, cv2.COLOR_BGR2RGBA)
+    #img2 = Image.fromarray(cv2image2)
+    #imgtk2 = ImageTk.PhotoImage(image=img2)
+    #output_image.imgtk = imgtk2
+    #output_image.configure(image=imgtk2)   
+    print('processing finish')   
     
     return closing
 
@@ -187,103 +210,139 @@ def counting(image):
     print('counting')
     
     if number == 1:
-        one = one +1
+        if start_stop.get() == 1:
+            one = one +1
         detected_number.config(text='1')
-    elif number == 2:    
-        two = two +1
+    elif number == 2:
+        if start_stop.get() == 1:
+            two = two +1
         detected_number.config(text='2')
-    elif number == 3:    
-        three = three +1
+    elif number == 3:
+        if start_stop.get() == 1:
+            three = three +1
         detected_number.config(text='3')
-    elif number == 4:    
-        four = four +1
+    elif number == 4:
+        if start_stop.get() == 1:        
+            four = four +1
         detected_number.config(text='4')
     elif number == 5:
-        five = five +1
+        if start_stop.get() == 1:        
+            five = five +1
         detected_number.config(text='5')
     elif number == 6:
-        six = six + 1
+        if start_stop.get() == 1:        
+            six = six + 1
         detected_number.config(text='6')
- #   elif number > 6 or number < 1:
-  #      cv2.imwrite(str(errorcnt) + 'error.png', grey)
-   #     errorcnt = errorcnt + 1
-    #    print('FEHLER')   
-     #   if errorcnt == 100:
-      #      errorcnt = 1
+    elif number > 6 or number < 1:
+        cv2.imwrite(str(errorcnt) + 'error.png', grey)
+        errorcnt = errorcnt + 1
+        print('BILDFEHLER')   
       
     rollnumber = rollnumber + 1  
     
-    all_numbers = [one, two, three, four, five, six, rollnumber, errorcnt]
+    all_numbers = [one, two, three, four, five, six, errorcnt,rollnumber]
     return all_numbers
     
-def logging(number):
+def logging(numbers):
     
     print('logging')
     
     file = open('log', 'w')
-    file.write('Einz:' + str(number[0]) + '\n')
-    file.write('Zwei:' + str(number[1]) + '\n')    
-    file.write("Drei: " + str(number[2]) + '\n')
-    file.write("Vier: " + str(number[3]) + '\n')
-    file.write("Fuenf: " + str(number[4]) + '\n')
-    file.write("Sechs: " + str(number[5]) + '\n')
-    file.write('Gesamt: ' + str(number[6]) + '\n')
-    file.write('Fehler: ' + str(number[7]) + '\n')
+    file.write('Einz:' + str(numbers[0]) + '\n')
+    file.write('Zwei:' + str(numbers[1]) + '\n')    
+    file.write("Drei: " + str(numbers[2]) + '\n')
+    file.write("Vier: " + str(numbers[3]) + '\n')
+    file.write("Fuenf: " + str(numbers[4]) + '\n')
+    file.write("Sechs: " + str(numbers[5]) + '\n')
+    file.write('Fehler: ' + str(numbers[6]) + '\n')
+    file.write('Gesamt: ' + str(numbers[7]) + '\n')
     file.close()   
   
-    values = [number[0],number[1],number[2],number[3],number[4],number[5]]
+    values = [numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5]]
     
     ax.cla()
+    ax.set_xlabel('Augenzahlen')
+    ax.set_ylabel('Häufigkeit')
     ax.bar([1,2,3,4,5,6], values)
-    canvas.draw()
-    print('processing finish')    
+ 
+    canvas1.draw()
+ 
+    ay.cla()
+    ay.set_xlabel('Wurfzahl')
+    ay.set_ylabel('Fehler')
+    ay.bar([1,2],(numbers[6],numbers[7]))
+    
+    canvas2.draw()
     
     
-def show_input(grey):
+ 
+    
+    
+def show_raw():
    
     #if bin_true.get() == 1:
     #    ret, grey = cv2.threshold(grey, int(binary_slider.get()) , 255, cv2.THRESH_BINARY)
         
-    cv2image1 = cv2.cvtColor(grey, cv2.COLOR_BGR2RGBA)
-    img1 = Image.fromarray(cv2image1)
-    imgtk1 = ImageTk.PhotoImage(image=img1)
-    raw_image.imgtk = imgtk1
-    raw_image.configure(image=imgtk1)
+    img2 = ImageTk.PhotoImage(Image.open('last_raw.png'))
+    raw_image.configure(image=img2)
+    raw_image.image = img2   
+        
+    #cv2image1 = cv2.cvtColor(grey, cv2.COLOR_BGR2RGBA)
+    #img1 = Image.fromarray(cv2image1)
+    #imgtk1 = ImageTk.PhotoImage(image=img1)
+    #raw_image.imgtk = imgtk1
+    #raw_image.configure(image=imgtk1)
  
-def show_output(img_with_keypoints):
+def show_output():
     
-    cv2image2 = cv2.cvtColor(img_with_keypoints, cv2.COLOR_BGR2RGBA)
-    img2 = Image.fromarray(cv2image2)
-    imgtk2 = ImageTk.PhotoImage(image=img2)
-    output_image.imgtk = imgtk2
-    output_image.configure(image=imgtk2)
-
+    img2 = ImageTk.PhotoImage(Image.open('last_output.png'))
+    output_image.configure(image=img2)
+    output_image.image = img2
+    
+    #cv2image2 = cv2.cvtColor(img_with_keypoints, cv2.COLOR_BGR2RGBA)
+    #img2 = Image.fromarray(cv2image2)
+    #imgtk2 = ImageTk.PhotoImage(image=img2)
+    #output_image.imgtk = imgtk2
+    #output_image.configure(image=imgtk2)
+    
+starting = 0
 def mainprogram():
     global stepper_running
-    
+    global taking_image
+    #global stepper_running
+    #global imshow_running
     #print('stepper_running: ' + str(stepper_running))
     #print('readyfi: ' + str(ready_for_img))
-    if start_stop.get() == 1:
-        if stepper_running == 0:
-            thread1 = stepperThread(1, "Thread-1", 1)
-            thread1.start()
-        if ready_for_img == 1:
-            
-            grey = get_image()
-            show_input(grey)
-            keypoint_img = img_processing(grey)
-            number = counting(keypoint_img)
-            logging(number)
-            show_output(keypoint_img)
-            stepper_running = 0
-            print('image finished')
+    if start_stop.get() == 1 and imgshow_running == 0:
+            if stepper_running == 0 and taking_image == 0:
+                stepper_running = 1
+                thread1 = stepperThread(1, "stepper Thread", 1)
+                thread1.start()
+            if stepper_running == 0 and taking_image == 1:
+                print('start processing')
+                grey = get_image()
+                show_raw()
+                keypoint_img = img_processing(grey)
+                show_output()
+                number = counting(keypoint_img)
+                logging(number)
+   
+                print('image finished')
+                taking_image = 0
         
         
     elif start_stop.get() == 0 and imgshow_running == 0:
-        thread2 = showInputThread(1, "Thread-1", 1)
+        thread2 = liveView(2, "liveView Thread", 2)
         thread2.start()
+
+        
+
     
-    topFrame.after(1000, mainprogram)  
+    
+    root.update()
+    root.after(1000, mainprogram)
+    
+
 
 
 topFrame = Frame(root)
@@ -322,15 +381,26 @@ detected_number = Label(topFrame, text='0', padx=50, pady=50)
 detected_number.config(font=("Courier", 44))
 detected_number.grid(row=0, column=2)
 
-fig = Figure()   
-ax = fig.add_subplot(111)
+fig1 = Figure()   
+ax = fig1.add_subplot(111)
 ax.set_xlabel('Augenzahlen')
 ax.set_ylabel('Häufigkeit')
-ax.grid
-        
-canvas = FigureCanvasTkAgg(fig, topFrame)
-canvas.get_tk_widget().grid(row=0, column=4)
-canvas.draw()
+
+
+canvas1 = FigureCanvasTkAgg(fig1, topFrame)
+canvas1.get_tk_widget().grid(row=0, column=4)
+canvas1.draw()
+
+
+fig2 = Figure()
+ay = fig2.add_subplot(111)
+ay.set_xlabel('Wurfzahl')
+ay.set_ylabel('Fehler')
+
+canvas2 = FigureCanvasTkAgg(fig2, topFrame)
+canvas2.get_tk_widget().grid(row=0, column=5)
+canvas2.draw()
+
 
 #image = get_image()
 #show_input(image)
